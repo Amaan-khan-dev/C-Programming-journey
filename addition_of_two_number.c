fba6b30af2db198b5d4a654854e954418a3d6a66
Amaan-khan-dev <mailtoamaandev@gmail.com>
2025-11-13 12:52:16 +0530

    third commit related 2 variable addition

---
+++ addition_of_two_number.c
@@ -0,0 +1,13 @@
+﻿#include<stdio.h>
+int main(){
+
+// Addition of two variable without taking input form user.
+
+int a = 50;
+int b = 50;
+int sum = a+b;
+
+printf("Sum of two number is: %d", sum);
+
+return 0;
+}
