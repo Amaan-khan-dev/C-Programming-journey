d800ea0b65ce63bba58182f4c0f608107f831226
Amaan-khan-dev <mailtoamaandev@gmail.com>
2025-11-15 14:22:19 +0530

    make simple intrest calculation programme in c

---
+++ Simple_intrest.c
@@ -0,0 +1,19 @@
+﻿#include <stdio.h>
+int main(){
+
+float p, r, t, si;
+	printf("Enter Principal: ");
+scanf("%f", &p);
+
+	printf("Enter rate of Intrest: ");
+scanf("%f", &r);
+
+	printf("Enter Times (in Year): ");
+scanf("%f", &t);
+
+  si = (p*r*t)/100;
+
+printf("Simple Intrest  = %.2f", si);
+
+return 0;
+}
