9b815417ddce918633a1146c52a54591680696ba
Amaan-khan-dev <mailtoamaandev@gmail.com>
2025-11-22 17:52:25 +0530

    modified user-table file

---
+++ input-from-user-table.c
@@ -0,0 +1,14 @@
+﻿#include<stdio.h>
+int main(){
+
+int i,num;
+
+printf("give number that you want to print table of that number: ");
+scanf("%d", &num);
+
+for(i=1;i<=10;i++){
+
+	printf("%d\n", i*num); \\print table 
+}
+return 0;
+}
