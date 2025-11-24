ed7f35ebcfc0183cbf8032519e05793927b0491c
Amaan-khan-dev <mailtoamaandev@gmail.com>
2025-11-24 18:01:17 +0530

    remainder related file

---
+++ remaninder1.c
@@ -0,0 +1,14 @@
+﻿#include<stdio.h>
+int main(){
+
+int a,b,q;
+	printf("Give Number a = ");
+	scanf("%d",&a);
+	printf("Give Number b = ");
+	scanf("%d",&b);
+q = a/b;   //qutient
+int r;
+r = a-(b*q); //remainder
+	printf("Remainder is:%d",r);
+return 0;
+}
