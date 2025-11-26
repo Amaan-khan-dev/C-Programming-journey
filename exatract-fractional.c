251c258652dd9f0eb74d0d5a2f3df2c854d23600
Amaan-khan-dev <mailtoamaandev@gmail.com>
2025-11-26 13:46:33 +0530

    Added fractional part extraction programme

---
+++ exatract-fractional.c
@@ -0,0 +1,18 @@
+﻿#include<stdio.h>
+int main(){
+
+// the programme is related that if one(float) number is given then
+//for example here 5.2
+//we want only it's fractional/point value
+//5.2 - 5 = 0.2 this is answer
+
+float num;
+	printf("Give a float Number:" );
+	scanf("%f", &num);
+int integer_number = num;
+
+float result =  num - integer_number;
+printf("Fractional part = %f", result);
+
+return 0;
+}
