4219e12fc4f4acc9fcb74c2bf7a256d005550bcd
Amaan-khan-dev <mailtoamaandev@gmail.com>
2025-11-27 15:53:19 +0530

    add biggest-number.c file

---
+++ Biggest_Number.c
@@ -0,0 +1,35 @@
+﻿#include<stdio.h> 
+int main(){
+
+    int num1;
+    int num2;
+    int num3;
+
+    printf("Give 3 Number and find which Number is biggest::\n");
+
+    printf("Enter a first Number: ");
+    scanf("%d", &num1);
+
+    printf("Enter a Secound Number: ");
+    scanf("%d", &num2);
+
+    printf("Enter a Third Number: ");
+    scanf("%d", &num3);
+
+    printf("1st number = %d 2nd number = %d 3rd number = %d\n", num1, num2, num3);
+
+    if(num1 > num2 && num1 > num3){
+        printf("first Number is biggest = %d", num1);
+    }
+    else if(num2 > num1 && num2 > num3){
+        printf("secound Number is biggest = %d", num2);
+    }
+    else if(num3 > num2 && num3 > num1){
+        printf("Third Number is biggest = %d", num3);
+    }
+    else{
+        printf("all number are equal");
+    }
+
+    return 0;
+}
---
+++ fractional.c
@@ -0,0 +1,20 @@
+﻿#include<stdio.h>
+int main(){
+
+// the programme is related that if one(float) number is give
+//for example here 5.2
+//we want only it's fractional/point value
+//5.2 - 5 = 0.2 this is answer
+
+    float num;
+    printf("Give a float Number: ");
+    scanf("%f", &num);
+
+    int integer_number = num;   // automatic conversion
+
+    float result = num - integer_number;
+    printf("Fractional part = %f", result);
+
+    return 0;
+}
+
